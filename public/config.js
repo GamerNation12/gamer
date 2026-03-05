c795923dd865314d0e20e1cf3a3779ad3238ffc4
Minecraft Gamer Nation <minecraft-gamer-nation@post.com>
2026-03-05 13:02:03 -0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzJkMjZjMmJlMDBjZmYwYTc4Njg2YWViMjI3MTYyMyIsIm5iZiI6MTc2MDQwMDM3OS43MjQsInN1YiI6IjY4ZWQ5M2ZiMGE0MGU3M2JmZGEyMWE5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1GzXIg3ybsXuiRRJFaJuokkoJ9KBGklxExp6fRSxV0E",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
